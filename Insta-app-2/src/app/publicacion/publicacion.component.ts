173f153356c1fb17c89a4af181e81d35c63c859f
Alan Santiago <alantadeo@live.com>
2021-10-03 19:22:17 -0500

    Clase del 28 de Septiembre

---
+++ Insta-app-2/src/app/publicacion/publicacion.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+
+@Component({
+  selector: 'app-publicacion',
+  templateUrl: './publicacion.component.html',
+  styleUrls: ['./publicacion.component.scss'],
+})
+export class PublicacionComponent implements OnInit {
+
+  idPublicacion: number;
+
+  constructor(private RutaActiva: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.idPublicacion = this.RutaActiva.snapshot.params.publicacionId;
+    console.log(this.idPublicacion);
+  }
+
+
+
+}
